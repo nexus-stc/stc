10f199453ee048388b6bcdff4f95fd44a8abaaca
the-superpirate <ultranymous@proton.me>
2023-04-27 14:33:40 +0300

    Add index selection, add timeout option

--- tools/stc_tools/cli.py
+++ tools/stc_tools/cli.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import json
 import sys
-from typing import Tuple
 
 import aiohttp
 import fire
@@ -27,15 +26,29 @@ class CidNotFound(Exception):
 
 
 class StcCliTools(StcTools):
-    async def search(self, query: str, index_name: str = 'nexus_science', limit: int = 1, default_fields = ('title', 'abstract',)):
+    default_default_fields = {
+        'nexus_science': ('title', 'abstract',),
+        'nexus_books': ('title',),
+    }
+
+    def __init__(
+        self,
+        ipfs_http_endpoint: str = 'http://127.0.0.1:8080',
+        index_name: str = 'nexus_science',
+        timeout: int = 600,
+    ):
+        super().__init__(ipfs_http_endpoint=ipfs_http_endpoint,
+                         paths=(f'/ipns/standard-template-construct.org/data/{index_name}/',))
+        self.index_name = index_name
+        self.timeout = timeout
+
+    async def search(self, query: str, limit: int = 1):
         """
         Searches in STC using default Summa match queries.
         Examples: `doi:10.1234/abc, isbns:9781234567890, "fetal hemoglobin"`
 
         :param query: query in Summa match format
-        :param index_name:
         :param limit: how many results to return, higher values incurs LARGE performance penalty.
-        :param default_fields:
 
         :return: metadata records
         """
@@ -43,36 +56,38 @@ class StcCliTools(StcTools):
         await self.setup()
         print(f"{colored('INFO', 'green')}: Searching {query}...")
         response = await super().search([{
-            "index_alias": index_name,
-            "query": {"query": {"match": {"value": query, "default_fields": default_fields, 'field_boosts': {}}}},
+            "index_alias": self.index_name,
+            "query": {"query": {
+                "match": {"value": query, "default_fields": self.default_default_fields[self.index_name],
+                          'field_boosts': {}}}},
             "collectors": [{"collector": {"top_docs": {"limit": limit}}}],
             "is_fieldnorms_scoring_enabled": False,
         }])
-        return list(map(lambda x: json.loads(x['document']), response[0]['collector_output']['documents']['scored_documents']))
+        return list(
+            map(lambda x: json.loads(x['document']), response[0]['collector_output']['documents']['scored_documents']))
 
-    async def download(self, query: str, output_path: str, index_name: str = 'nexus_science'):
+    async def download(self, query: str, output_path: str):
         """
         Download file from STC using default Summa match queries.
         Examples: `doi:10.1234/abc, isbns:9781234567890`
 
         :param query: query in Summa match format
         :param output_path: filepath for writing file
-        :param index_name:
 
         :return: file if record has corresponding CID
         """
-        results = await self.search(query, index_name=index_name)
+        results = await self.search(query)
         if results:
             print(f"{colored('INFO', 'green')}: Found {query}")
             if 'cid' in results[0]:
                 print(f"{colored('INFO', 'green')}: Receiving file {query}...")
-                async with aiohttp.ClientSession() as session:
+                async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=self.timeout)) as session:
                     async with session.get(f'{self.ipfs_http_endpoint}/ipfs/{results[0]["cid"]}') as resp:
                         data = await resp.read()
                         with open(output_path, 'wb') as f:
                             f.write(data)
                             f.close()
-                            print(f'File {output_path} is written')
+                            print(f"{colored('INFO', 'green')}: File {output_path} is written")
             else:
                 print(f"{colored('ERROR', 'red')}: Not found CID for {query}", file=sys.stderr)
         else:
@@ -81,9 +96,17 @@ class StcCliTools(StcTools):
 
 async def stc_tools_cli(
     ipfs_http_endpoint: str = 'http://127.0.0.1:8080',
-    paths: Tuple[str] = ('/ipns/standard-template-construct.org/data/nexus_science/',),
+    index_name: str = 'nexus_science',
+    timeout: int = 600
 ):
-    stc_tools_client = StcCliTools(ipfs_http_endpoint, paths)
+    """
+
+    :param ipfs_http_endpoint: IPFS HTTP API Endpoint
+    :param index_name: `nexus_books` (similar to LibGen) or `nexus_science` (similar to Crossref)
+    :param timeout: timeout for requests to IPFS
+    :return:
+    """
+    stc_tools_client = StcCliTools(ipfs_http_endpoint, index_name, timeout)
     return {
         'search': stc_tools_client.search,
         'download': stc_tools_client.download,
